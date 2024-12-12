c470292cef29968104e797ab01b966d54976bde5
MicHub-ux <m.cetraro@studenti.unisa.it>
2024-12-12 16:33:47 +0100

    Major changes on Contact class: - Added invariants - Added toString() method - Added implementation for already prototyped methods

--- src/main/java/it/unisa/diem/gruppo9/logic/Contact.java
+++ src/main/java/it/unisa/diem/gruppo9/logic/Contact.java
@@ -60,78 +60,110 @@ public class Contact {
      *
      *
      *
-     * @pre PhoneNumber must not have more than three entries
-     * @pre Email must not have more than three entrie
-     * @pre At least one between name and surnam must not be empty
+     * @invariant PhoneNumber must not have more than three entries and it must
+     * not be null
+     * @invariant Email must not have more than three entrie and it must not be
+     * null
+     * @invariant At least one between name and surname must not be empty
+     *
+     * @pre An instance of {@code Contact} has been declared and not yet
+     * initialized
+     * @pre The number and type of parameters needed to invoke this constructor
+     * is coherent with its declaration
+     * @pre All entries in the phoneNumber and email {@code Array} must not be
+     * empty so that blank entries will still be recognizible with a "-"
+     * character.
+     *
+     * @post Every parameter is not modified or casted to another type
      * @post A new contact object is created with the specified attributes
      * @post If one between name and surname is empty it is initialized as empty
      * String
-     * @post If no phone numbers are inserted the attribute phoneNumber is
-     * initialized as an empty {@code Array}
-     * @post If no emails are inserted the attribute email is initialized as an
-     * empty {@code Array}
-     *
-     *
-     *
      *
      * @param name the first name of the contact.
      * @param surname the last name of the contact.
      * @param phoneNumber an array of phone numbers, up to three.
      * @param email an array of email addresses, up to three.
      *
-     *
+     * @throws RuntimeException if one of the parameters doesn't respect the
+     * invariants
+     * @throws NullPointerException if one of the arrays doesn't respect the
+     * invariants
      */
     public Contact(String name, String surname, String[] phoneNumber, String[] email) {
+        if (name.isEmpty() && surname.isEmpty()) {
+            throw new RuntimeException("A contact must have at least a name or a surname.");
+        } else {
+
+            this.name = name;
+            this.surname = surname;
+
+            if (phoneNumber.length <= 3) {
+                this.phoneNumber = phoneNumber;
+            } else if (phoneNumber == null) {
+                throw new NullPointerException("Phone number array cannot be null.");
+            } else {
+                throw new RuntimeException("A contact must have less than four phone numbers");
+            }
+            if (email.length <= 3) {
+                this.email = email;
+            } else if (email == null) {
+                throw new NullPointerException("Email array cannot be null.");
+            } else {
+                throw new RuntimeException("A contact must have less than four emails");
+            }
+        }
     }
 
     /**
      * Returns the first name of the contact.
      *
-     * @pre None(it can be called anytime)
-     * @post Return the value of the name attribute
+     * @pre The {@code Contact} must be properly initialized
+     * @post Return the value of the name attribute without editing it
      * @post If the name is empty returns null
      *
      * @return the first name of the contact, or {@code null} if it is empty.
      */
     public String getName() {
+        return this.name;
     }
 
     /**
      * Returns the last name of the contact.
      *
-     * @pre None(it can be called anytime)
-     * @post Return the value of the surname attribute
+     * @pre The {@code Contact} must be properly initialized
+     * @post Return the value of the surname attribute without editing it
      * @post If the surname is empty returns null
      *
      * @return the last name of the contact, or {@code null} if it is empty.
      */
     public String getSurname() {
+        return this.surname;
     }
 
     /**
      * Returns the array of phone numbers associated with the contact.
      *
-     * @pre None(it can be called anytime)
-     * @post Returns the array of phone numbers
+     * @pre The {@code Contact} must be properly initialized
+     * @post Returns the array of phone numbers without editing it
      * @post If no phone numbers are set it return null
      *
-     *
-     * @return an array of phone numbers, or {@code null} if no numbers are set.
+     * @return an array of phone numbers, or the blank characters "-"
      */
     public String[] getPhoneNumber() {
+        return this.phoneNumber;
     }
 
     /**
      * Returns the array of email addresses associated with the contact.
      *
-     * @pre None(it can be called anytime)
-     * @post Return the array pf email
+     * @pre The {@code Contact} must be properly initialized
+     * @post Return the array of email without editin it
      * @post If no emails are set it return null
      *
-     * @return an array of email addresses, or {@code null} if no addresses are
-     * set.
+     * @return an array of email addresses, or the blank characters "-"
      */
     public String[] getEmail() {
+        return this.email;
     }
 
     /**
@@ -142,9 +174,10 @@ public class Contact {
      * @post if name is an empty {@code String}, the name attribute became an
      * empty{@code String}
      *
-     * @param name the new first name of the contact.
+     * @param name the new name of the contact.
      */
     public void setName(String name) {
+        this.name = name;
     }
 
     /**
@@ -155,9 +188,10 @@ public class Contact {
      * @post if surname is an empty {@code String}, the surname attribute became
      * an empty {@code String}
      *
-     * @param surname the new last name of the contact.
+     * @param surname the new surname of the contact.
      */
     public void setSurname(String surname) {
+        this.surname = surname;
     }
 
     /**
@@ -167,13 +201,24 @@ public class Contact {
      * initializes it if it is currently empty.
      * </p>
      *
-     * @pre phone number must not have more than three entries
-     * @pre phone number must not be null
-     * @post the phone number is updated to the specified array
+     * @invariant {@code phoneNumber} must not have more than three entries and
+     * it must not be null
+     * @post the {@code phoneNumber} is updated to the specified array
      *
      * @param phoneNumber the new array of phone numbers, up to three entries.
+     * @throws RuntimeException if the {@code phoneNumber} {@code Array} exceeds
+     * the maximum size allowed
+     * @throws NullPointerException if the {@code Array} is null
      */
     public void setPhoneNumber(String[] phoneNumber) {
+        if (phoneNumber == null) {
+            throw new NullPointerException("Phone number array cannot be null.");
+        }
+        if (phoneNumber.length <= 3) {
+            this.phoneNumber = phoneNumber;
+        } else {
+            throw new RuntimeException("A contact must have less than four phone numbers");
+        }
     }
 
     /**
@@ -183,13 +228,75 @@ public class Contact {
      * or initializes it if it is currently empty.
      * </p>
      *
-     * @pre email must not have more than three entries
-     * @pre email must not be null
-     * @post the email is updated to the specified array
+     * @invariant {@code email} must not have more than three entries and it
+     * must not be null
+     * @post the {@code email} is updated to the specified array
      *
      * @param email the new array of email addresses, up to three entries.
+     * @throws RuntimeException if the {@code email} {@code Array} exceeds the
+     * maximum size allowed
+     * @throws NullPointerException if the {@code Array} is null
      */
     public void setEmail(String[] email) {
+        if (email == null) {
+            throw new NullPointerException("Email array cannot be null.");
+        }
+        if (email.length <= 3) {
+            this.email = email;
+        } else {
+            throw new RuntimeException("A contact must have less than four emails.");
+        }
+    }
+    /**
+     * Returns a string representation of the {@code Contact} object in a
+     * specific format.
+     * <p>
+     * The string representation of a {@code Contact} object includes the
+     * following attributes, separated by semicolons:
+     * <ul>
+     * <li>{@code surname}: the last name of the contact;</li>
+     * <li>{@code name}: the first name of the contact;</li>
+     * <li>{@code phoneNumber}: a semicolon-separated list of all phone numbers
+     * associated with the contact;</li>
+     * <li>{@code email}: a semicolon-separated list of all email addresses
+     * associated with the contact.</li>
+     * </ul>
+     * A newline character is appended at the end of the string.
+     * </p>
+     * <p>
+     * If the {@code phoneNumber} or {@code email} arrays contain multiple
+     * elements, they will be concatenated into a single string using semicolons
+     * as the delimiter. If either array is empty, the corresponding part of the
+     * string will be left blank.
+     * </p>
+     *
+     * @invariant The {@code surname}, {@code name}, {@code phoneNumber}, and
+     * {@code email} fields must not be null. The arrays {@code phoneNumber} and
+     * {@code email} must contain at most three elements.
+     *
+     * @pre The {@code Contact} object must be properly initialized with valid
+     * attributes. - {@code surname} and {@code name} must not be null. -
+     * {@code phoneNumber} and {@code email} must not be null and must contain
+     * valid entries.
+     *
+     * @post The returned string will contain: - The {@code surname} followed by
+     * a semicolon. - The {@code name} followed by a semicolon. - The
+     * concatenated {@code phoneNumber} entries, separated by semicolons. - The
+     * concatenated {@code email} entries, separated by semicolons. - A newline
+     * character at the end of the string.
+     *
+     * @return a string representation of the {@code Contact} object, formatted
+     * as:
+     * {@code "surname;name;phoneNumber1;phoneNumber2;...;email1;email2;...\n"}
+     * where {@code phoneNumber1}, {@code phoneNumber2}, etc., and
+     * {@code email1}, {@code email2}, etc., are the entries in the respective
+     * arrays.
+     */
+    @Override
+    public String toString() {
+        String phoneNumbers = String.join(";", phoneNumber);
+        String emails = String.join(";", email);
+        return this.surname + ";" + this.name + ";" + phoneNumbers + ";" + emails + "\n";
     }
 
 }
