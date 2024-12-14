1ff965406eda53caeda93ac3e5945c0cef47659b
Gaetano <gaetanocarbone2004@gmail.com>
2024-12-14 10:08:52 +0100

    - Added BricoRubrica class implementation - Changes on AddView.fxml file

--- src/main/java/it/unisa/diem/gruppo9/main/BricoRubrica.java
+++ src/main/java/it/unisa/diem/gruppo9/main/BricoRubrica.java
@@ -1,34 +1,37 @@
 package it.unisa.diem.gruppo9.main;
 
+import it.unisa.diem.gruppo9.interfaces.controller.PrimaryDisplayController;
+import it.unisa.diem.gruppo9.logic.ContactManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/** @mainpage 
-  * <p> 
-  * Application "BricoRubrica" developed by:
-  * <ul>
-  *     <li> {@author Michele Cetraro - 0612707419} </li>
-  *     <li> {@author Mario Pellegrino Ambrosone - 0612707417} </li>
-  *     <li> {@author Simone Grimaldi - 0612707338} </li>
-  *     <li> {@author Gaetano Carbone - 0612707663} </li>
-  * </ul>
-  * </p>
-  * 
-  * <p>
-  * The "BricoRubrica" is an AddressBook-like software that provides multiple data structures in order
-  * to memorize and operate on a list of contacts via GUI, as well as an explictly-user-activated mechanism 
-  * that enables to overwrite a file with all the information contained in the address book. 
-  * Each {@code Contact} is identified by a set of informations and
-  * each {@code Contact} is contained in a {@code ContactManager} object.
-  * </p>
+/**
+ * @mainpage <p>
+ * Application "BricoRubrica" developed by:
+ * <ul>
+ *     <li> {@author Michele Cetraro - 0612707419} </li>
+ *     <li> {@author Mario Pellegrino Ambrosone - 0612707417} </li>
+ *     <li> {@author Simone Grimaldi - 0612707338} </li>
+ *     <li> {@author Gaetano Carbone - 0612707663} </li>
+ * </ul>
+ * </p>
+ *
+ * <p>
+ * The "BricoRubrica" is an AddressBook-like software that provides multiple data structures in order
+ * to memorize and operate on a list of contacts via GUI, as well as an explictly-user-activated mechanism
+ * that enables to overwrite a file with all the information contained in the address book.
+ * Each {@code Contact} is identified by a set of informations and
+ * each {@code Contact} is contained in a {@code ContactManager} object.
+ * </p>
  */
- /**
+
+/**
  * @file BricoRubrica.java
  * @brief The main entry point for the BricoRubrica application.
- * 
+ *
  * <p>
  * {@code BricoRubrica} extends the {@link javafx.application.Application} class to provide the 
  * JavaFX application lifecycle. It initializes and displays the primary stage of the application 
@@ -39,7 +42,7 @@ import javafx.stage.Stage;
  * and displaying the initial view. The {@link #main(String[])} method serves as the entry point 
  * for the JVM, while the {@link #start(Stage)} method initializes the JavaFX stage.
  * </p>
- * 
+ *
  * @author gruppo9
  * @version 1.0
  * @since 2024-12-07
@@ -54,12 +57,25 @@ public class BricoRubrica extends Application {
      * {@link javafx.stage.Stage} of the application. It loads the FXML file representing the 
      * primary user interface, creates a {@link javafx.scene.Scene}, and sets it on the stage.
      * </p>
-     * 
+     *
      * @param stage the primary stage for this application, provided by the JavaFX runtime.
      * @throws Exception if an error occurs during FXML loading or stage setup.
+     *
+     * @pre the file {@code PrimaryView.fxml} must exisist and be accesibile
+     *
+     * @post the current scene is replaced with {@code PrimaryView.fxml}.
      */
     @Override
     public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/it/unisa/diem/gruppo9/interfaces/view/PrimaryView.fxml"));
+        Parent root = loader.load();
+        PrimaryDisplayController controller = loader.getController();
+        controller.setContacts(new ContactManager());  // Passa una nuova istanza di ContactManager
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     /**
@@ -68,7 +84,7 @@ public class BricoRubrica extends Application {
      * This method calls the {@link #launch(String[])} method to start the JavaFX runtime 
      * and subsequently invoke the {@link #start(Stage)} method.
      * </p>
-     * 
+     *
      * @param args command-line arguments passed to the application.
      */
     public static void main(String[] args) {
